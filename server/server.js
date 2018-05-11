ca25e0832dddd67f8db4a2bdbbaa4ac0cf8595da
Antonio Cozzi <antoniocozzi2@gmail.com>
2018-05-11 22:46:21 +0200

    Emit new message on createMessage

--- server/server.js
+++ server/server.js
@@ -7,20 +7,33 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
-var io = socketIO(server);         //ritorna direttamente un websocket
+var io = socketIO(server); //ritorna direttamente un websocket
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-     console.log('New user connected');
+    console.log('New user connected');
 
-     socket.on('disconnect', () => {
-          console.log('Client disconnected');
-     });
+    socket.emit('newMessage', {
+        from: 'nikel@example.com',
+        text: 'Are you ready for rocking?',
+        createdAt: new Date().getTime()
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log('New message ', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
 
-
-
 server.listen(port, () => {
-     console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
